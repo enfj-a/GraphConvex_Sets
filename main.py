5a6ed6748203b7277e700c4b81dda145088849da
enfj-a <umich.raghav@gmail.com>
2024-05-23 02:24:27 -0400

    Created  main.py
    #Co-authored-by: RaghavM11 <mishraraghav111@gmail.com>

---
+++ main.py
@@ -0,0 +1,8 @@
+## shortest-paths-in-graphs-of-convex-sets
+
+##This repository contains extended python code on 2d tunnel which is further converted in c++. GCC code is taken from paper [**"Shortest Paths in Graphs of Convex Sets" by Tobia Marcucci, Jack Umenberger, Pablo A. Parrilo, and Russ Tedrake**](https://arxiv.org/abs/2101.11565).
+
+##TODO:
+
+##Further ablation study wrt GUROBI and Mosek Optimizer.
+
